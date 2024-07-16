29b596c90cb922a33565ad39b88c88c91f9b6aa1
Adam Erikuly <adamerikoff@protonmail.com>
2024-07-16 17:00:14 +0500

    Create Point.py

---
+++ Point.py
@@ -0,0 +1,36 @@
+class Point(object):
+    def __init__(self, x, y, a, b):
+        self.x = x
+        self.y = y
+        self.a = a
+        self.b = b
+        if self.x is None and self.y is None:
+            return
+        if self.y ** 2 != self.x ** 3 + a * x + b:
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+
+    def __eq__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s ** 2 - self.x - other.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+        if self == other:
+            s = (3 * self.x ** 2 + self.a) / (2 * self.y)
+            x = s ** 2 - 2 * self.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
+    def __ne__(self, other):
+        return not (self == other)
+
